213fc32cf6a09d356656bd537169ce05e037d894
frandrew90 <frandrewjs@gmail.com>
2021-10-20 00:26:39 +0300

    made hw6 step 2

--- src/Redux/phonebook/phonebook-actions.js
+++ src/Redux/phonebook/phonebook-actions.js
@@ -1,33 +1,62 @@
+// import { v4 as uuidv4 } from 'uuid';
+// import {
+//   ADD_CONTACT,
+//   GET_CONTACTS,
+//   REMOVE_CONTACT,
+//   FIND_CONTACT,
+// } from './phonebook-types';
+
+// export const addNewContact = (name, number) => {
+//   return {
+//     type: ADD_CONTACT,
+//     payload: { id: uuidv4(), name, number },
+//   };
+// };
+
+// export const getAllContacts = contacts => {
+//   return {
+//     type: GET_CONTACTS,
+//     payload: contacts,
+//   };
+// };
+
+// export const removeContact = id => {
+//   return {
+//     type: REMOVE_CONTACT,
+//     payload: id,
+//   };
+// };
+
+// export const findContact = value => ({
+//   type: FIND_CONTACT,
+//   payload: value,
+// });
+
+// ===================TOOLKIT==================================================================
+import { createAction } from '@reduxjs/toolkit';
 import { v4 as uuidv4 } from 'uuid';
-import {
-  ADD_CONTACT,
-  GET_CONTACTS,
-  REMOVE_CONTACT,
-  FIND_CONTACT,
-} from './phonebook-types';
+// import {
+//   ADD_CONTACT,
+//   GET_CONTACTS,
+//   REMOVE_CONTACT,
+//   FIND_CONTACT,
+// } from './phonebook-types';
 
-export const addNewContact = (name, number) => {
-  return {
-    type: ADD_CONTACT,
-    payload: { id: uuidv4(), name: name, number: number },
-  };
-};
+export const addNewContact = createAction(
+  'phonebook/addContact',
+  (name, number) => {
+    return {
+      payload: {
+        id: uuidv4(),
+        name,
+        number,
+      },
+    };
+  },
+);
 
-export const getAllContacts = contacts => {
-  return {
-    type: GET_CONTACTS,
-    payload: contacts,
-  };
-};
+export const getAllContacts = createAction('phonebook/getContacts');
 
-export const removeContact = id => {
-  return {
-    type: REMOVE_CONTACT,
-    payload: id,
-  };
-};
+export const removeContact = createAction('phonebook/removeContact');
 
-export const findContact = value => ({
-  type: FIND_CONTACT,
-  payload: value,
-});
+export const findContact = createAction('phonebook/findContact');
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -12,12 +12,16 @@
 
 // ===================TOOLKIT==============================
 
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 
 import phonebookReducer from './phonebook/phonebook-reducers';
 
+const middleware = [...getDefaultMiddleware(), logger];
+
 const store = configureStore({
   reducer: { contacts: phonebookReducer },
+  middleware,
   devTools: process.env.NODE_ENV === 'development',
 });
 
