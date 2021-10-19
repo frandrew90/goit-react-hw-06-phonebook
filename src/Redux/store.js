bb855835dbd4f56775b949e0915cc410551aa95e
frandrew90 <frandrewjs@gmail.com>
2021-10-19 23:25:26 +0300

    made refactoring store

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,11 +1,24 @@
-import { createStore, combineReducers } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { createStore, combineReducers } from 'redux';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+// import phonebookReducer from './phonebook/phonebook-reducers';
+
+// const rootReducer = combineReducers({
+//   contacts: phonebookReducer,
+// });
+
+// const store = createStore(rootReducer, composeWithDevTools());
+
+// export default store;
+
+// ===================TOOLKIT==============================
+
+import { configureStore } from '@reduxjs/toolkit';
+
 import phonebookReducer from './phonebook/phonebook-reducers';
 
-const rootReducer = combineReducers({
-  contacts: phonebookReducer,
+const store = configureStore({
+  reducer: { contacts: phonebookReducer },
+  devTools: process.env.NODE_ENV === 'development',
 });
-
-const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
